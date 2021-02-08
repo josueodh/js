4d66f43fc94efa08f6fd148499c827b883f0ce69
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-02-08 18:55:27 -0300

    adicionando controllers

--- 04-primeiro-projeto-com-node-js/src/modules/users/infra/http/routes/sessions.routes.ts
+++ 04-primeiro-projeto-com-node-js/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,24 +1,10 @@
 import { Router } from 'express';
 
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import { container } from 'tsyringe';
+import SessionsController from '../controllers/SessionsController';
 
+const sessionsController = new SessionsController();
 const sessionsRouter = Router();
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-  const usersRepository = new UsersRepository();
 
-  const authenticateUser = container.resolve(AuthenticateUserService);
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.status(200).json({ user, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
--- 04-primeiro-projeto-com-node-js/src/modules/users/infra/http/routes/user.routes.ts
+++ 04-primeiro-projeto-com-node-js/src/modules/users/infra/http/routes/user.routes.ts
@@ -2,49 +2,22 @@ import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import { container } from 'tsyringe';
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-  const usersRepository = new UsersRepository();
-  const { name, password, email } = request.body;
-
-  const createUser = container.resolve(CreateUserService);
-
-  const user = await createUser.execute({
-    name,
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json(user);
-});
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    const usersRepository = new UsersRepository();
-
-    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-
-    return response.json(user);
-  },
+  userAvatarController.update,
 );
 
 export default usersRouter;
