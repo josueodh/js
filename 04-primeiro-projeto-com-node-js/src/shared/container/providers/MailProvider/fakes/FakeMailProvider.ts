0ca70ec6ea4660e80f3b92ef24a1ff24566ce452
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-02-10 12:09:30 -0300

    Finalizando template de E-mail com Engine

--- 04-primeiro-projeto-com-node-js/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
+++ 04-primeiro-projeto-com-node-js/src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -1,15 +1,9 @@
+import ISendMailDTO from '../dtos/ISendMailDTO';
 import IMailProvider from '../models/IMailProvider';
 
-interface IMessage {
-  to: string;
-  body: string;
-}
 export default class FakeMailProvider implements IMailProvider {
-  private messages: IMessage[] = [];
-  public async sendMail(to: string, body: string): Promise<void> {
-    this.messages.push({
-      to,
-      body,
-    });
+  private messages: ISendMailDTO[] = [];
+  public async sendMail(message: ISendMailDTO): Promise<void> {
+    this.messages.push(message);
   }
 }
--- 04-primeiro-projeto-com-node-js/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ 04-primeiro-projeto-com-node-js/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,9 +1,16 @@
 import nodemailer, { Transporter } from 'nodemailer';
+import { inject, injectable } from 'tsyringe';
+import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 import IMailProvider from '../models/IMailProvider';
 
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mainTemplateProvider: IMailTemplateProvider,
+  ) {
     nodemailer.createTestAccount().then(account => {
       const transporter = nodemailer.createTransport({
         host: account.smtp.host,
@@ -17,12 +24,23 @@ export default class EtherealMailProvider implements IMailProvider {
       this.client = transporter;
     });
   }
-  public async sendMail(to: string, body: string): Promise<void> {
+  public async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
     const message = await this.client.sendMail({
-      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
-      to,
-      subject: 'Recuperação de senha',
-      text: body,
+      from: {
+        name: from?.name || 'Equipe GoBarber',
+        address: from?.email || 'equipe@gobarber.com',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mainTemplateProvider.parse(templateData),
     });
 
     console.log('Message sent: %s', message.messageId);
