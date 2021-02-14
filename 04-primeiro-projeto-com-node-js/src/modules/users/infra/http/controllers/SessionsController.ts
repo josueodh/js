c7a79ba78f0814a50bef0dd8caab1cd28b3987da
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-02-13 22:50:47 -0300

    Utilizando Class Transformer

--- 04-primeiro-projeto-com-node-js/src/modules/users/infra/http/controllers/SessionsController.ts
+++ 04-primeiro-projeto-com-node-js/src/modules/users/infra/http/controllers/SessionsController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 import UsersRepository from '../../typeorm/repositories/UsersRepository';
-
+import { classToClass } from 'class-transformer';
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
@@ -15,8 +15,6 @@ export default class SessionsController {
       password,
     });
 
-    delete user.password;
-
-    return response.status(200).json({ user, token });
+    return response.status(200).json({ user: classToClass(user), token });
   }
 }
