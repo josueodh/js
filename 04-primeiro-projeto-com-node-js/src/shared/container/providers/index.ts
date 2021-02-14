3a6eaffc430fea93703c7a3d175467698941fece
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-02-14 15:34:40 -0300

    Emails pelo Amazon SES

--- 04-primeiro-projeto-com-node-js/src/shared/container/providers/index.ts
+++ 04-primeiro-projeto-com-node-js/src/shared/container/providers/index.ts
@@ -1,9 +1,12 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
+
 import IMailProvider from './MailProvider/models/IMailProvider';
 
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
@@ -23,7 +26,9 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver == 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
 
 container.registerSingleton<INotificationsRepository>(
