cac004a7296d717f93cc2244f4497e3a8a0781bd
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-02-14 16:16:39 -0300

    Organizando o container

---
+++ 04-primeiro-projeto-com-node-js/src/shared/container/providers/MailTemplateProvider/index.ts
@@ -0,0 +1,12 @@
+import { container } from 'tsyringe';
+import IMailTemplateProvider from './models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './implementations/HandlebarsMailTemplateProvider';
+
+const provider = {
+  handlebars: HandlebarsMailTemplateProvider,
+};
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  provider.handlebars,
+);
