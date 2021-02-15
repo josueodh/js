7e9148f43212077b8f7826ec59d23a5091effbf9
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-02-15 16:24:08 -0300

    Serialização do Cache

--- 04-primeiro-projeto-com-node-js/src/modules/appointments/infra/http/controllers/ProviderAppontmentsController.ts
+++ 04-primeiro-projeto-com-node-js/src/modules/appointments/infra/http/controllers/ProviderAppontmentsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+import { classToClass } from 'class-transformer';
 
 export default class ProviderAppontmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -20,6 +21,6 @@ export default class ProviderAppontmentsController {
       provider_id,
     });
 
-    return response.json(appointment);
+    return response.json(classToClass(appointment));
   }
 }
