4aec3b1889aa7edc1eacf8c80b15a4f951ddd08b
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-02-17 15:12:24 -0300

    Página de perfil

--- 04-primeiro-projeto-com-node-js/src/shared/container/providers/StorageProvider/index.ts
+++ 04-primeiro-projeto-com-node-js/src/shared/container/providers/StorageProvider/index.ts
@@ -5,7 +5,7 @@ import DiskStorageProvider from './implementations/DiskStorageProvider';
 import S3StorageProvider from './implementations/S3StorageProvider';
 
 const providers = {
-  diskStorage: DiskStorageProvider,
+  disk: DiskStorageProvider,
   s3: S3StorageProvider,
 };
 
