bf6f7ef9ac32f2bdca0989a0579e7321ec840130
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-02-17 15:36:49 -0300

    Troca de avatar

--- 06-gobarber-web/src/pages/Profile/index.tsx
+++ 06-gobarber-web/src/pages/Profile/index.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useRef } from "react";
-import { FiMail, FiLock, FiUser, FiCamera, FiArrowLeft } from "react-icons/fi";
+import React, { ChangeEvent, useCallback, useRef } from "react";
+import { FiUser, FiMail, FiLock, FiCamera, FiArrowLeft } from "react-icons/fi";
 import { FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
 import * as Yup from "yup";
@@ -7,14 +7,15 @@ import { Link, useHistory } from "react-router-dom";
 
 import api from "../../services/api";
 
+import { useAuth } from "../../hooks/auth";
 import { useToast } from "../../hooks/toast";
-import getValidationErros from "../../utils/getValidationErrors";
+
+import getValidationErrors from "../../utils/getValidationErrors";
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 import { Container, Content, AvatarInput } from "./styles";
-import { useAuth } from "../../hooks/auth";
 
 interface ProfileFormData {
   name: string;
@@ -27,44 +28,73 @@ const Profile: React.FC = () => {
   const { addToast } = useToast();
   const history = useHistory();
 
-  const { user } = useAuth();
+  const { user, updateUser } = useAuth();
+
   const handleSubmit = useCallback(
     async (data: ProfileFormData) => {
       try {
         formRef.current?.setErrors({});
+
         const schema = Yup.object().shape({
           name: Yup.string().required("Nome obrigatório"),
           email: Yup.string()
-            .required("E-mail obrigatório")
-            .email("E-mail inválido"),
+            .email("Digite um e-mail válido")
+            .required("E-mail obrigatório"),
           password: Yup.string().min(6, "No mínimo 6 dígitos"),
         });
+
         await schema.validate(data, {
           abortEarly: false,
         });
 
         await api.post("/users", data);
+
         history.push("/");
+
         addToast({
           type: "success",
           title: "Cadastro realizado!",
-          description: "Você já pode fazer logon no GoBarber",
+          description: "Você já pode fazer seu logon no GoBarber!",
         });
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
-          const errors = getValidationErros(err);
+          const errors = getValidationErrors(err);
+
           formRef.current?.setErrors(errors);
+
           return;
         }
+
         addToast({
           type: "error",
-          title: "Erro na autenticação",
-          description: "Ocorreu um erro ao fazer cadastro, tente novamente",
+          title: "Erro no cadastro",
+          description: "Ocorreu um erro ao fazer cadastro, tente novamente.",
         });
       }
     },
     [addToast, history]
   );
+
+  const handleAvatarChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        const data = new FormData();
+
+        data.append("avatar", e.target.files[0]);
+
+        api.patch("/users/avatar", data).then((response) => {
+          updateUser(response.data);
+
+          addToast({
+            type: "success",
+            title: "Avatar atualizado!",
+          });
+        });
+      }
+    },
+    [addToast, updateUser]
+  );
+
   return (
     <Container>
       <header>
@@ -74,44 +104,53 @@ const Profile: React.FC = () => {
           </Link>
         </div>
       </header>
+
       <Content>
         <Form
+          ref={formRef}
           initialData={{
             name: user.name,
             email: user.email,
           }}
-          ref={formRef}
           onSubmit={handleSubmit}
         >
           <AvatarInput>
             <img src={user.avatar_url} alt={user.name} />
-            <button type="button">
+            <label htmlFor="avatar">
               <FiCamera />
-            </button>
+
+              <input type="file" id="avatar" onChange={handleAvatarChange} />
+            </label>
           </AvatarInput>
-          <h1>Meu Perfil</h1>
+
+          <h1>Meu perfil</h1>
+
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
+
           <Input
             containerStyle={{ marginTop: 24 }}
-            type="old_password"
-            name="password"
+            name="old_password"
             icon={FiLock}
+            type="password"
             placeholder="Senha atual"
           />
+
           <Input
-            type="password"
             name="password"
             icon={FiLock}
+            type="password"
             placeholder="Nova senha"
           />
+
           <Input
-            type="password_confirmation"
-            name="password"
+            name="password_confirmation"
             icon={FiLock}
+            type="password"
             placeholder="Confirmar senha"
           />
-          <Button type="submit">Confirmar Mudanças</Button>
+
+          <Button type="submit">Confirmar mudanças</Button>
         </Form>
       </Content>
     </Container>
