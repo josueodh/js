80159a3ffd67d177f34b2d674c764417150823ed
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-02-21 15:41:52 -0300

    Formatando a data

--- appgobarber/src/pages/AppointmentCreated/index.tsx
+++ appgobarber/src/pages/AppointmentCreated/index.tsx
@@ -1,6 +1,7 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useCallback } from 'react';
-
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useCallback, useMemo } from 'react';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import Icon from 'react-native-vector-icons/Feather';
 import {
   Container,
@@ -10,8 +11,13 @@ import {
   OkButtonText,
 } from './styles';
 
+interface RouteParams {
+  date: number;
+}
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation();
+  const { params } = useRoute();
+  const routeParams = params as RouteParams;
   const handleOkPressed = useCallback(() => {
     reset({
       routes: [
@@ -22,11 +28,19 @@ const AppointmentCreated: React.FC = () => {
       index: 0,
     });
   }, []);
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE',dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm 'h",
+      { locale: ptBR },
+    );
+  }, [routeParams.date]);
   return (
     <Container>
       <Icon name="check" size={80} color="#04d361" />
       <Title>Agendamento concluído</Title>
-      <Description />
+      <Description>{formattedDate}</Description>
       <OkButton onPress={handleOkPressed}>
         <OkButtonText>Ok</OkButtonText>
       </OkButton>
