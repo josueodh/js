9a80286bfb9e741042452581be818cc9c8ad3b8c
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-04-07 15:13:23 -0300

    Enviando dashgo e iniciando ignews

---
+++ Ignite/react/ignews/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,12 @@
+import styled from "./styles.module.scss";
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  return (
+    <button type="button" className={styled.subscribeButton}>
+      Subscribe now
+    </button>
+  );
+}
